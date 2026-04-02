760e30005ab91de96fa82bf0d4f6b53c4d07a384
yeshuaimmie <85786885+yeshuaimmie@users.noreply.github.com>
2026-04-02 17:10:10 +0300

    Align withdrawal minimum with UI

--- src/validations/withdrawalValidation.js
+++ src/validations/withdrawalValidation.js
@@ -1,7 +1,7 @@
 const { body } = require('express-validator');
 
 exports.createWithdrawalValidation = [
-  body('amount').isFloat({ min: 5000 }).withMessage('Minimum withdrawal is UGX 5,000'),
+  body('amount').isFloat({ min: 10000 }).withMessage('Minimum withdrawal is UGX 10,000'),
   body('paymentMethod').isIn(['mtn', 'airtel', 'bank']).withMessage('Invalid payment method'),
   body('phone').trim().isLength({ min: 10 }).withMessage('Phone number is required'),
   body('accountName').trim().isLength({ min: 2 }).withMessage('Account name is required'),
