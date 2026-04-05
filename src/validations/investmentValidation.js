2382fac31784abb839f27c4d7bb02f862feab450
yeshuaimmie <85786885+yeshuaimmie@users.noreply.github.com>
2026-04-05 08:22:17 +0300

    Lower investment minimum to UGX 30,000

--- src/validations/investmentValidation.js
+++ src/validations/investmentValidation.js
@@ -2,5 +2,5 @@ const { body } = require('express-validator');
 
 exports.createInvestmentValidation = [
   body('planId').isMongoId().withMessage('Invalid investment plan'),
-  body('amount').isFloat({ min: 50000 }).withMessage('Minimum investment is UGX 50,000'),
+  body('amount').isFloat({ min: 30000 }).withMessage('Minimum investment is UGX 30,000'),
 ];
